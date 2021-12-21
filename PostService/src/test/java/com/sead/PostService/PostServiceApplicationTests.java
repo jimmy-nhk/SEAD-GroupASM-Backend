1ceac20277dd35e5c171a2627e1511095d17ab44
Hieu Pham <harrios8451@gmail.com>
2021-12-21 21:02:09 +0700

    initial post commit

---
+++ PostService/src/test/java/com/sead/PostService/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sead.PostService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
